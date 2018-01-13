3d3e63ada4ca298a62f9e69f56d0d9a7ae8439cc
4loops <colpeterson@gmail.com>
2018-01-13 15:01:31 -0800

    simplified extraction file

--- save_activations.py
+++ save_activations.py
@@ -6,10 +6,7 @@ import torchvision.transforms as transforms
 from torch.autograd import Variable
 from PIL import Image
 
-import os
-import cPickle as pickle
 
-import sys
 
 model = alexnet(pretrained=True)
 model.eval() #so dropout is fixed
@@ -22,48 +19,6 @@ pre_transform=transforms.Compose([
     torchvision.transforms.ToTensor(),
     normalize])
 
-images = []
-paths  = []
-directory = '.'
-save_destination = 'activations'
-
-for filename in os.listdir(directory):
-    filepath = os.path.join(directory,filename)
-    if filename.endswith(".jpg"):
-        print 'reading image', filepath
-        temp = Image.open(filepath)
-        im = temp.copy()
-        temp.close()
-        images.append(im)
-        paths.append(filepath)
-
-
-print "TOTAL NUMBER OF IMAGES LOADED:", len(images)
-
-#normalization and preprocessing
-images = map(pre_transform,images)
-
-#check for greyscale images
-for i, im in enumerate(images):
-    if im.size(0) == 1:
-        images[i] = im.expand(3,224,224)
-
-#mega_batch = torch.zeros((len(images),3,224,224))
-#for i, im in enumerate(images):
-#    mega_batch[i] = im
-#mega_batch = Variable(mega_batch)
-print "FINSHED LOADING IMAGES."
-
-def layer_name(l):
-    name = str(l)
-    index = name.index('(')
-    return name[:index].strip()
-
-for i, input in enumerate(images):
-    in_ = Variable(input.unsqueeze(0)) 
-    print "evalutating:", paths[i]
-    out = model(in_)
-
 def extract_activatons(filename):
     temp = Image.open(filepath)
     im = temp.copy()
@@ -75,7 +30,6 @@ def extract_activatons(filename):
     in_ = Variable(im.unsqueeze(0))
     return model(in_)
 
-
-
-
+if __name__ == "__main__":
+    extract_activatons("bee.jpg")
 
