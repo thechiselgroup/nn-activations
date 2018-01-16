a5569b19aca3451dac68cd2ac7930b8661dcdf8a
Meagan <meagan.jensen@icloud.com>
2018-01-16 14:02:07 -0800

    modifications to make it work

--- save_activations.py
+++ save_activations.py
@@ -1,29 +1,30 @@
 import torch
 import torchvision
 from alexnet import alexnet
+import sys
+import os
 
 import torchvision.transforms as transforms
 from torch.autograd import Variable
 from PIL import Image
-
-
 
 model = alexnet(pretrained=True)
 model.eval() #so dropout is fixed
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
 
-pre_transform=transforms.Compose([
-    transforms.Scale(256),
+pre_transform = transforms.Compose([
+    transforms.Resize(256),
     transforms.CenterCrop(224),
     torchvision.transforms.ToTensor(),
     normalize])
 
-def extract_activatons(filename):
-    temp = Image.open(filepath)
+def extract_activations(filename):
+    temp = Image.open(filename)
+    print temp.format
     im = temp.copy()
     temp.close()
-    im = pretransform(im)
+    im = pre_transform(im)
     #for greyscale images
     if im.size(0) == 1:
         images[i] = im.expand(3,224,224)
@@ -31,5 +32,15 @@ def extract_activatons(filename):
     return model(in_)
 
 if __name__ == "__main__":
-    extract_activatons("bee.jpg")
+    if(len(sys.argv) < 3):
+        print "Usage: save_activations.py <image path> <destination path>"
+        sys.exit(1)
+
+    try:
+        os.makedirs(sys.argv[2])
+    except OSError:
+        if not os.path.isdir(sys.argv[2]):
+            raise
+
+    extract_activations(sys.argv[1])
 
--- wrapper.py
+++ wrapper.py
@@ -1,19 +1,26 @@
+import sys
+import numpy as np
+
 class Decorator():
-    
+
     def __init__(self,input_id="test"):
         self.count = 0
         self.input_id = input_id
+
     def reset_count(self):
         self.count = 0
 
     def torch_decorator(self, fun):
         def wrapper(*args,**kwargs):
             r = fun(*args,**kwargs)
-            self.count += 1 
+            self.count += 1
+            #TODO what is the best way of taking the arg originally and passing it in as a filename?
             #fun.__name__ will be a string like ReLU or conv that identifies the type of function
-            filename = "out/"+str(self.count)+"_"+fun.__name__+"_"+self.input_id
+            filename = sys.argv[2]+""+str(self.count)+"_"+fun.__name__+"_"+self.input_id
+
             #this can be replaced with any side effect
-            r.data.numpy().tofile(filename)
+            np.save(filename, r.data.numpy())
+
             return r
         return wrapper
 
